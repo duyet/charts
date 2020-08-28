3911fb3cea456089d9c0dc728955dc5f2499d930
Duyet Le <lvduyet@fossil.com>
2020-08-28 18:25:42 +0700

    Update

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -x
 
+DIST_DIR=".dist"
+
 for chart in ./*; do
     if [ -f "$chart/Chart.yaml" ]; then
-        helm package $chart -d dist
+        helm package $chart -d $DIST_DIR
     fi
 done
 
-helm repo index --url https://duyet.github.io/charts/dist ./dist
-cp ./dist/index.yaml ./index.yaml
+helm repo index --url https://duyet.github.io/charts/dist $DIST_DIR
+cp $DIST_DIR/index.yaml ./index.yaml
