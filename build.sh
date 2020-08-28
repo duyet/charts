07a7a3d8ca57b3814f5a19042891ccfba9bce9c2
Van-Duyet Le <5009534+duyet@users.noreply.github.com>
2020-08-28 21:37:20 +0700

    Auto build and release (#5)
    Co-authored-by: GitHub Action <action@github.com>

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+DIST_DIR=.dist
+
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
+for chart in ./*; do
+    if [ -f "$chart/Chart.yaml" ]; then
+        helm dependency update $chart
+        helm dependency build $chart
+        helm package $chart -d $DIST_DIR
+    fi
+done
+
+helm repo index --url https://duyet.github.io/charts/$DIST_DIR $DIST_DIR
+cp $DIST_DIR/index.yaml ./index.yaml
