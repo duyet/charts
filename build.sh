61228bde373ef70cd80215515f514c6275f77b1c
Duyet Le <lvduyet@fossil.com>
2020-08-28 21:02:42 +0700

    Update build script

--- build.sh
+++ build.sh
@@ -3,8 +3,12 @@ set -x
 
 DIST_DIR=dist
 
+helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add bitnami https://charts.bitnami.com/bitnami
+
 for chart in ./*; do
     if [ -f "$chart/Chart.yaml" ]; then
+        mkdir $chart/charts
         helm dependency update $chart
         helm dependency build $chart
         helm package $chart -d $DIST_DIR
