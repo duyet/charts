6401b29b8478c709d0f5ff675d52e05947dd195f
Duyet Le <lvduyet@fossil.com>
2020-08-28 18:43:21 +0700

    Update dependency

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ DIST_DIR=".dist"
 
 for chart in ./*; do
     if [ -f "$chart/Chart.yaml" ]; then
+        helm dependency update $chart
         helm dependency build $chart
         helm package $chart -d $DIST_DIR
     fi
 done
 
-helm repo index --url https://duyet.github.io/charts/dist $DIST_DIR
+helm repo index --url https://duyet.github.io/charts/$DIST_DIR $DIST_DIR
 cp $DIST_DIR/index.yaml ./index.yaml
