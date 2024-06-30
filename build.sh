15f5e794c0a8eb5441c28877ce7e3877f9444cb5
Duyet Le <me@duyet.net>
2024-06-30 14:49:23 +0700

    chore(ci): update stable repo

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -x
 
 DIST_DIR=.dist
 
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
+helm repo add stable https://charts.helm.sh/stable
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
 for chart in ./*; do
